863e168db8ab244dd496952957c9d8409046b491
Thiago Costa <thiago@redemidiainterativa.com.br>
2022-07-20 10:09:34 -0300

    puxando as empresas em veículos e substituindo input pelo select

--- resources/frontend/angular/src/app/vehicle-list/vehicle-list.component.ts
+++ resources/frontend/angular/src/app/vehicle-list/vehicle-list.component.ts
@@ -4,6 +4,8 @@ import { ActivatedRoute } from '@angular/router';
 import { Vehicle } from '../vehicles';
 import { VehicleService } from "../vehicle.service";
 import { getResultXML } from "../../assets/functions";
+import {Company} from "../companies";
+import {CompanyService} from "../company.service";
 
 @Component({
   selector: 'app-vehicle-list',
@@ -12,17 +14,24 @@ import { getResultXML } from "../../assets/functions";
 })
 export class VehicleListComponent {
   vehicles!: Vehicle[];
+  companies!: Company[];
   companyId: number;
   editVehicle: Vehicle | undefined;
 
   constructor(
     private route: ActivatedRoute,
-    private vehicleService: VehicleService
+    private vehicleService: VehicleService,
+    private companyService: CompanyService
   ) {
     this.companyId = Number(this.route.snapshot.paramMap.get('companyId'));
     this.vehicleService.getVehicles(this.companyId)
       .subscribe((data) => {
         this.vehicles = getResultXML(data, 'getVehiclesResult');
+      })
+
+    this.companyService.getAllCompanies()
+      .subscribe((data) => {
+        this.companies = getResultXML(data, 'getCompaniesResult');
       })
   }
 
